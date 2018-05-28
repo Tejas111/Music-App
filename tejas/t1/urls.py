2a95ff784fc9b4929f88558a9791f4e41512f470
Tejas111 <tejaskumark11@gmail.com>
2018-05-28 17:05:46 +0530

    new changes

--- tejas/t1/urls.py
+++ tejas/t1/urls.py
@@ -2,8 +2,8 @@ from django.urls import re_path,path
 from . import views
 app_name='t1'
 urlpatterns=[
-    path('',views.ind),
-    re_path(r'^(?P<album_id>[0-9]+)/$',views.detail,name='detail'),
-    re_path(r'^(?P<album_id>[0-9]+)/favorite/$',views.favorite,name='favorite'),
+    path('',views.IndexView.as_view(),name='index'),
+    re_path(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name='detail'),
+    #re_path(r'^(?P<album_id>[0-9]+)/favorite/$',views.favorite,name='favorite'),
 
 ]
--- tejas/t1/views.py
+++ tejas/t1/views.py
@@ -1,26 +1,12 @@
-from django.shortcuts import render,get_object_or_404
-
-# Create your views here.
-from django.http import HttpResponse
-from django.template import loader
+from django.views import generic
 from .models import album
-def ind (request):
-    album1 = album.objects.all()
-    return render(request,'t1/index.html',{'all_albums':album1})
-    #template=loader.get_template("t1/index.html")
-   
 
-def detail(request,album_id):
-    album1 = get_object_or_404(album,pk=album_id)
-    return render(request,'t1/detail.html',{'album':album1})
+class IndexView(generic.ListView):
+    template_name= 't1/index.html'
+    def get_queryset(self):
+        return album.objects.all()
 
-def favorite(request,album_id):
-    album1= get_object_or_404(album,pk=album_id)
-    try:
-        s_song= album1.song_set.get(pk=request.POST['song'])
-    except (KeyError):
-        return render(request,'t1/detail.html',{'album':album1,'error_message':"No song is selected"})
-    else:
-        s_song.is_favorite=True
-        s_song.save()
-        return render(request,'t1/detail.html',{'album':album1})
+class DetailView(generic.DetailView):
+    model = album
+    template_name='t1/index.html'
+
